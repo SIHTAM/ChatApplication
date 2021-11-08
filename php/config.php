4d1c0679de67733ad90fa7ec5a8f015f6cc198bb
SIHTAM <56382532+SIHTAM@users.noreply.github.com>
2021-11-08 10:24:27 +0100

    Included Open Database Connectivity ODBC Interface.

--- php/config.php
+++ php/config.php
@@ -1,12 +1,7 @@
 <?php
 /* Database credentials. */
-define('DB_SERVER', '127.0.0.1:3306');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'test');
-
 try{
-    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    $pdo = new PDO("odbc:MySqlTest");
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
